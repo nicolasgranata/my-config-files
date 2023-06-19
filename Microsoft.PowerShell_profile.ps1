c48d220347a334d8adaaf36aa0befc82b8c42157
Nico Granata <nicolasegranata@gmail.com>
2023-06-19 00:24:59 -0300

    Added Terminal-Icons check

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,10 @@
 oh-my-posh --init --shell pwsh --config "$env:POSH_THEMES_PATH/nicolasgranata.shell.omp.json" | Invoke-Expression
 
-Import-Module -Name Terminal-Icons
+if (Get-Module -ListAvailable -Name Terminal-Icons) {
+    Import-Module -Name Terminal-Icons
+} 
+else {
+    Write-Host "Terminal-Icons Module does not exist" -ForegroundColor Red
+    Write-Host "Installing Terminal-Icons Module..." -ForegroundColor Green
+    Install-Module -Name Terminal-Icons -Repository PSGallery -Scope CurrentUser
+}
